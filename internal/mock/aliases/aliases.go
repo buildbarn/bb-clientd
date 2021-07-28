b2e479a485d7695720d3cc95c6e23aafe677409a
Ed Schouten <eschouten@apple.com>
2021-07-28 18:08:22 +0200

    Tidy up the way we declare mocks
    Instead of manually adding aliases, we can just use the gomock()
    mock_names parameter. This simplifies the story around cross
    compilation.

--- internal/mock/aliases/aliases.go
+++ internal/mock/aliases/aliases.go
@@ -2,9 +2,6 @@ package aliases
 
 import (
 	"io"
-
-	"github.com/buildbarn/bb-clientd/pkg/outputpathpersistency"
-	"github.com/buildbarn/bb-remote-execution/pkg/filesystem/fuse"
 )
 
 // This file contains aliases for some of the interfaces provided by the
@@ -12,24 +9,6 @@ import (
 // gomock() Bazel rule to emit mocks for them, as that rule is only
 // capable of emitting mocks for interfaces built through a
 // go_library().
-//
-// It also contains aliases for some of the interfaces provided by the
-// FUSE package. These aliases are used to rename them to prevent naming
-// collisions with other interface types for which we want to generate
-// mocks.
-
-// FUSEDirectory is an alias of fuse.Directory.
-type FUSEDirectory = fuse.Directory
-
-// FUSELeaf is an alias of fuse.Leaf.
-type FUSELeaf = fuse.Leaf
-
-// OutputPathPersistencyReadCloser is an alias of
-// outputpathpersistency.ReadCloser.
-type OutputPathPersistencyReadCloser = outputpathpersistency.ReadCloser
-
-// OutputPathPersistencyStore is an alias of outputpathpersistency.Store.
-type OutputPathPersistencyStore = outputpathpersistency.Store
 
 // ReaderAt is an alias of io.ReaderAt.
 type ReaderAt = io.ReaderAt
